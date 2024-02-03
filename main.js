cd97bc6d8efd808292bcdd40ec22589aac58faa3
rakib-hasan12 <rakibthesr3@gmail.com>
2024-02-03 20:53:37 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// let type= new Typed('#changing-text', {
+//     string: ['Web Designer','youtuber','ftft'],
+//     typeSpeed:50,
+//     backSpeed:50,
+//     loop: true,
+// })
+
+
+let type= new Typed('#changing-text',{
+    strings : ['Web-Designer','Web-Developer'],
+    typeSpeed :50,
+    backSpeed :60,
+    loop : true
+})
