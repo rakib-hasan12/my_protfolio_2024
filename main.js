bc43da8b154aa78864aa57608a36266ee3494feb
rakib-hasan12 <rakibthesr3@gmail.com>
2024-02-08 16:00:37 +0600

    port-3

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// let type= new Typed('#changing-text', {
-//     string: ['Web Designer','youtuber','ftft'],
-//     typeSpeed:50,
-//     backSpeed:50,
-//     loop: true,
-// })
-
-
 let type= new Typed('#changing-text',{
     strings : ['Web-Designer','Front-End Developer','Student'],
     typeSpeed :80,
